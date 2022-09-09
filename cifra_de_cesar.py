0320a4805b56035c44a6889fd39fe203d7fda4cb
Giovanni Fossati <giovannifossati11@gmail.com>
2022-09-09 10:56:52 -0300

    redefinindo variaveis e utilizações de lower/upper

--- cifra_de_cesar.py
+++ cifra_de_cesar.py
@@ -3,14 +3,12 @@ import PySimpleGUI as sg
 nome = sg.popup_get_text("Qual seu nome? ", title="Vamos nos conhecer!", text_color = 'black', background_color = 'RoyalBlue', location = (500,250))
 sg.popup(f"Olá {nome}, você sabe o que é cifra de cesar? \nÉ um tipo de cifra de substituição na qual cada letra do texto é substituída por outra, que se apresenta no alfabeto abaixo dela um número fixo de vezes. Por exemplo, com uma troca de três posições, A seria substituído por D, B se tornaria E, e assim por diante.", title="Boas Vindas!", text_color = 'black', background_color = 'RoyalBlue', location = (500,250))
 
-frase = sg.popup_get_text("OBS: Não utilizar caracteres especiais. \nPor gentileza, digite a frase: ", title="Escolha uma frase!",text_color = 'black', background_color = 'RoyalBlue', location = (500,250))
-fraseFormatadaArray = list(frase.upper())
+frase = sg.popup_get_text("OBS: Não utilizar caracteres especiais. \nPor gentileza, digite a frase: ", title="Escolha uma frase!",text_color = 'black', background_color = 'RoyalBlue', location = (500,250)).upper()
+fraseFormatadaArray = list(frase)
 
-chave = sg.popup_get_text("Digite a chave para encriptação: ", title="Escolha da chave.", text_color = 'black', background_color = 'RoyalBlue', location = (500,250))
-chave = int(chave)
+chave = int(sg.popup_get_text("Digite a chave para encriptação: ", title="Escolha da chave.", text_color = 'black', background_color = 'RoyalBlue', location = (500,250)))
 
-escolha = sg.popup_get_text("Deseja encriptar ou decriptar sua mensagem? \nDigite 1 para encriptar; \nDigite 2 para decriptar;", title="Eis a questão...", text_color = 'black', background_color = 'RoyalBlue', location = (500,250))
-escolha = int(escolha)
+escolha = int(sg.popup_get_text("Deseja encriptar ou decriptar sua mensagem? \nDigite 1 para encriptar; \nDigite 2 para decriptar;", title="Eis a questão...", text_color = 'black', background_color = 'RoyalBlue', location = (500,250)))
 
 alfabeto = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',' '] #Como convertemos toda a frase para letras em maiúsculo podemos definir somente as letras em maiúsculo do alfabeto
 
@@ -27,8 +25,8 @@ if 0 < chave < 26:
                 nova_Posicao = posicao + chave
             caractere_criptografado = alfabeto[nova_Posicao % len(alfabeto)]
             fraseConvertidaArray.append(caractere_criptografado)
-            fraseConvertidaString = ''.join(fraseConvertidaArray)
-            fraseImpressaUsuario = fraseConvertidaString.lower()
+            fraseImpressaUsuario = ''.join(fraseConvertidaArray).lower()
+
         sg.popup("Sua frase encriptada é: ", fraseImpressaUsuario, title="Encriptada", text_color = 'black', background_color = 'RoyalBlue', location = (500,250))
       
     elif escolha == 2:
@@ -41,8 +39,7 @@ if 0 < chave < 26:
                 nova_Posicao = posicao - chave
             caractere_decriptado = alfabeto[nova_Posicao % len(alfabeto)]
             fraseConvertidaArray.append(caractere_decriptado)
-            fraseConvertidaString = ''.join(fraseConvertidaArray)
-            fraseImpressaUsuario = fraseConvertidaString.lower()
+            fraseImpressaUsuario = ''.join(fraseConvertidaArray).lower()
         sg.popup("Sua frase decriptada é: ", fraseImpressaUsuario, title="Decriptada", text_color = 'black', background_color = 'RoyalBlue', location = (500,250))
 
     else:
